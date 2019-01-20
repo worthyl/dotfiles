70176c0583cbb2cf94acfc96333c80975c1f9af3
Worthy LaFollette <worthyl@gmail.com>
2019-01-20 16:08:57 -0600

    added scripts

---
+++ scripts/cvid.sh
@@ -0,0 +1,7 @@
+fullfile=$1
+filename=$(basename "$fullfile")
+extension="${filename##*.}"
+filename="${filename%.*}"
+echo "$fullfile", "$filename.mp4"
+exec ffmpeg -protocol_whitelist "file,tls,tcp,https" -i "$fullfile" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "$filename.mp4"
+#ffmpeg -protocol_whitelist "file,tls,tcp,https" -i "$fullfile" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "$filename.mp4"
