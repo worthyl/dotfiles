83057139c6b19f4609dd3fa74484aff25a7bd945
Worthy LaFollette <worthylafollette@Worthys-MacBook-Pro.local>
2024-12-24 18:37:07 -0600

    test

--- cvid.sh
+++
@@ -1,6 +0,0 @@
-fullfile=$1
-filename=$(basename "$fullfile")
-extension="${filename##*.}"
-filename="${filename%.*}"
-echo "$fullfile", "$filename.mp4"
-exec ffmpeg -protocol_whitelist "file,tls,tcp,https" -i "$fullfile" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "$filename.mp4"
